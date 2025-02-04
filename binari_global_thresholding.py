c87431b1b06b327c0168c1bdc43678caf4358e26
Ryuchanghoon <fbckdgns3@naver.com>
2025-02-05 02:12:15 +0900

    [#2] add: 이진화 Otsu 임계값 출력 기능

--- binari_global_thresholding.py
+++ binari_global_thresholding.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 image_path = "test.jpg"
@@ -13,6 +14,23 @@ combined_image = np.hstack((image, simple_binary))
 
 cv2.namedWindow("Manual Thresholding", cv2.WINDOW_NORMAL)
 cv2.imshow('Manual Thresholding', combined_image)
+
+# ------ Otsu's Thresholding -------
+
+_, otsu_binary = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+hist = cv2.calcHist([image], [0], None, [256], [0, 256])
+
+
+plt.plot(hist, color='black')
+plt.title('Grayscale Histogram')
+plt.xlabel('Pixel Value')
+plt.ylabel('Frequency')
+plt.show()
+
+
+cv2.namedWindow('Otsu thresholding', cv2.WINDOW_NORMAL)
+cv2.imshow('Otsu thresholding', otsu_binary)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
