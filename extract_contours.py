9cd1090b12e9fa768fb3ab5bfc67d6e87702fd9e
Ryuchanghoon <fbckdgns3@naver.com>
2025-02-21 00:25:36 +0900

    [#2] feat: 배경 제거 기능 세팅

---
+++ extract_contours.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+
+image = cv2.imread("reduced_shadow.jpg", cv2.IMREAD_GRAYSCALE)
+
+mean_bin = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 51, 15)  # Mean Adaptive Thresholding
+mean_bin_inv = 255 - mean_bin  # 흑백 반전
+
+contours, _ = cv2.findContours(mean_bin_inv, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+largest_contour = max(contours, key=cv2.contourArea)
+
+x, y, w, h = cv2.boundingRect(largest_contour)  # 좌표값 추출
+
+filtered_image = np.zeros_like(image)
+
+filtered_image[y:y+h, x:x+w] = image[y:y+h, x:x+w]  # 기존 이미지 => 배경 제거 이미지 덮어쓰기
+
+
+cv2.namedWindow('delete background', cv2.WINDOW_NORMAL)
+cv2.imshow('delete background', filtered_image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
