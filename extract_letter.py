c83a896bc490a32f26d1220a24224e698626600f
Ryuchanghoon <fbckdgns3@naver.com>
2025-03-04 16:42:34 +0900

    add: paddleOCR 추가

--- extract_letter.py
+++ extract_letter.py
@@ -1,11 +1,25 @@
 import cv2
 import easyocr as ocr
+from paddleocr import PaddleOCR
 
 
-img = cv2.imread('test.jpg')
+img = cv2.imread('improve_OCR_processor/test.jpg')
+
+# ------ easyOCR -------
 
 reader = ocr.Reader(['ko', 'en'], gpu = False)
 
 text = reader.readtext(img, detail = 0)
 
 print(text)
+
+
+# ------ paddleOCR -------
+
+paocr = PaddleOCR(lang = 'korean')
+
+results = paocr.ocr(img, cls = True)
+
+text_only = [line[1][0] for res in results for line in res]
+
+print(text_only)
